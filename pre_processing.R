a4df196c857caa35c8a07d5de5207ab2b20982d2
frank-britto <54486706+frank-britto@users.noreply.github.com>
2024-12-24 21:02:25 -0500

    Update pre_processing.R

--- pre_processing.R
+++ pre_processing.R
@@ -1,97 +1,3 @@
-# To-do
-# https://cran.r-project.org/web/packages/docstring/vignettes/docstring_intro.html
-# Generate documentation for each function in the package
-
-excel2tibble <- function(name) {
-  
-  # ######################################################################## 
-  # Description: process data from TECAN* from a SINGLE strain. It           
-  # assumes that we are working inside the folder where the data is located  
-  #                                                                         
-  # Inputs:                                                                  
-  #         name:         name of the Excel file's path <type:string>         
-  #                                                                          
-  # Output:                                                                  
-  #         tidy:  output tidy dataframe <type:tibble>                       
-  #                                                                          
-  # Comments: this is the type of data used for making all plots.            
-  # ######################################################################## 
-  
-  # Vector to store the different inductor concentrations
-  iptg <- c()
-  
-  # Obtain the strain name from the file
-  file_name <- basename(name)  
-  components <- unlist(strsplit(file_name, "_"))  
-  strain_name <- components[1]
-  
-  # Processing each Excel tab
-  sheets <- strsplit(excel_sheets(name), split = " ")
-  for (j in seq_along(1:length(sheets))){
-    iptg <- append(iptg, as.numeric(sheets[[j]][1])*1000)
-  }
-  
-  for (ind in seq_along(1:length(iptg))) {
-    
-    # Obtaining the index of the empty column
-    idx <- NA
-    tab <- read_excel(name, sheet = excel_sheets(name)[ind])
-    for (i in seq_along(colnames(tab))){
-      if (substring(colnames(tab)[i],1,1) == '.') {
-        idx <- i
-        break
-      }
-    }
-    
-    # Get the OD and fluorescence data
-    t <- tab %>% select(colnames(tab)[1])
-    od <- tab %>% select(colnames(tab)[2:as.integer(idx-1)]) %>% mutate_all(~.-0.1)
-    flu <- tab %>% select(colnames(tab)[as.integer(idx+2): length(colnames(tab))])
-    
-    # Calculate the derived variables
-    phi <- flu / od
-    diff_od <- apply(od, 2, diff)
-    diff_flu <- apply(flu, 2, diff)
-    diff_t <- apply(t, 2, diff)
-    gr <- diff_od / (od * diff_t)
-    pr <- diff_flu / (od * diff_t)
-    
-    # Applying mean
-    od <- od %>% apply(1,mean)
-    flu <- flu %>% apply(1,mean)
-    phi <- phi %>% apply(1,mean)
-    gr <- gr %>% apply(1,mean)
-    pr <- pr %>% apply(1,mean)
-    
-    # Generate matrix with all this data
-    base <- bind_cols(t, od, flu, phi, gr, pr) %>% 
-      mutate(inductor = iptg[ind]) %>%
-      set_names(c("time", "od", "flu", "phi", "gr", "pr", "inductor"))
-    
-    if (ind == 1){
-      bases <- base
-    } 
-    else{
-      bases <- bind_rows(bases, base)
-    }
-    
-    tidy <- tibble(
-      time = bases$time,
-      od = bases$od,
-      flu = bases$flu,
-      phi = bases$phi,
-      growth_rate = bases$gr,
-      production_rate = bases$pr,
-      Condition = bases$inductor,
-      strain = strain_name
-    )
-    
-  }
-  
-  return(tidy)
-  
-}
-
 block_shape_processing <- function(name) {
   
   
@@ -169,39 +75,54 @@ block_shape_processing <- function(name) {
   
 }
 
-tidy_shape_processing <- function(name) {
+smooth_filter <- function(df, window_size = 5) {
   
-  # ######################################################################## 
-  # Description: tidy-shape to wide-shape format  
-  #                                                                         
-  # Inputs:                                                                  
-  #         name:         name of the Excel file's path <type:string>         
-  #                                                                          
-  # Output:                                                                  
-  #         raw_data:     output tidy dataframe <type:tibble>                                  
-  # ########################################################################
+  # Check if 'gr' and 'pr' columns exist, if not, create them
+  if (!'gr' %in% colnames(df)) {
+    df$gr <- NA
+  }
+  if (!'pr' %in% colnames(df)) {
+    df$pr <- NA
+  }
   
-  # To-Do
-  # General idea:
-  # Recreate raw_data from previous function
-  # Find the unique(name$wells) values
-  # Iterate through them, look into the raw_data columns and do
-  # raw_data[column] <- name %>% filter(Condition == iteration)[data]
+  # Perform rolling window calculation for the growth rate and production rate
+  for (i in 1:(nrow(df) - window_size + 1)) {
+    # Get the window of data
+    window_data <- df[i:(i + window_size - 1), ]
+    
+    # Linear regression model for 'od' as dependent variable
+    model1 <- lm(od ~ time, data = window_data)
+    growth_rate <- coef(model1)[2]
+    
+    # Linear regression model for 'flu' as dependent variable
+    model2 <- lm(flu ~ time, data = window_data)
+    production_rate <- coef(model2)[2]
+    
+    # Assign the computed rates to the corresponding indices
+    df[i, 'gr'] <- growth_rate
+    df[i, 'pr'] <- production_rate
+  }
   
+  # Normalization
+  df$gr <- df$gr / df$od
+  df$pr <- df$pr / df$od
+  
+  # Returning the dataframe with the new growth rate (gr) and production rate (pr) columns
+  return(df)
 }
 
 raw2tidy <- function(od_matrix, flu_matrix, design_matrix, blank_matrix) {
   
-  # Step N°1: pre-allocation
+  # Step N°1: Pre-allocation
   
-  # Extracting the number of experimental conditions besides the blank
+  # Extract the number of experimental conditions besides the blank
   design_conditions <- design_matrix$Condition
   unique_conditions <- unique(design_conditions[!is.na(design_conditions) & design_conditions != "blank"])
   
-  # Calculating the total number of rows
-  num_rows <- length(od_matrix$time) * length(unique_conditions)
+  # Calculate the total number of rows
+  num_rows <- length(od_matrix$time) * length(unique_conditions) * max(table(design_matrix$Condition))
   
-  # Pre-allocation in a new dataframe
+  # Pre-allocate a new dataframe
   tidy_format <- data.frame(
     time = rep(od_matrix$time, length.out = num_rows),
     od = rep(NA, num_rows),
@@ -209,35 +130,36 @@ raw2tidy <- function(od_matrix, flu_matrix, design_matrix, blank_matrix) {
     growth_rate = rep(NA, num_rows),
     production_rate = rep(NA, num_rows),
     phi = rep(NA, num_rows),
-    Condition = rep(rep(unique_conditions, each = length(od_matrix$time)), times = 1)
+    Condition = rep(rep(unique_conditions, each = length(od_matrix$time)), times = max(table(design_matrix$Condition))),
+    replicate = rep(NA, num_rows)
   )
   
-  # Step N°2: blank substraction and averaging
+  # Step N°2: Blank subtraction and assigning replicate numbers
   
   # Create a dataframe to store od and flu values separately
   od_values <- tibble(!!!setNames(replicate(length(unique_conditions), rep(NA, length(od_matrix$time)), simplify = FALSE), unique_conditions))
   flu_values <- tibble(!!!setNames(replicate(length(unique_conditions), rep(NA, length(flu_matrix$time)), simplify = FALSE), unique_conditions))
   
-  # Iterating through the "Condition" column of blank_matrix
+  # Iterate through the "Condition" column of blank_matrix
   for (blank_condition in unique(blank_matrix$Condition)) {
     
     # Save the corresponding well coordinate
     assigned_blank <- blank_matrix$Wells[blank_matrix$Condition == blank_condition]
     
-    # Look to which wells does the blank correspond for further subtraction
+    # Look to which wells the blank corresponds for further subtraction
     matching_rows <- design_matrix$Well[!is.na(design_matrix$Condition) & design_matrix$Condition == blank_condition]
     corresponding_wells <- unique(matching_rows)
     
     # Check for > 1 replicates
     if (length(corresponding_wells) > 1) {
       
-      # Broadcast assigned_blank column to match the length of corresponding_wells columns
+      # Broadcasting and subtracting blank values for multiple replicates
       broadcasted_assigned_blank <- od_matrix[, assigned_blank, drop = FALSE]
       broadcasted_assigned_blank <- cbind(broadcasted_assigned_blank, od_matrix[, assigned_blank, drop = FALSE][, rep(1, length(corresponding_wells)-1)])
       subtracted_columns <- od_matrix[, corresponding_wells, drop = FALSE] - broadcasted_assigned_blank
       average_subtracted_columns <- rowMeans(subtracted_columns, na.rm = TRUE)
       
-      # Add to od_values values
+      # Assign the subtracted values
       od_values[[blank_condition]] <- average_subtracted_columns
       
       # Repeat for fluorescent data
@@ -247,43 +169,52 @@ raw2tidy <- function(od_matrix, flu_matrix, design_matrix, blank_matrix) {
       average_subtracted_columns <- rowMeans(subtracted_columns, na.rm = TRUE)
       flu_values[[blank_condition]] <- average_subtracted_columns
       
-    } 
-    
-    else {
+    } else {
       # Broadcasting and appending without averaging
       broadcasted_assigned_blank <- od_matrix[, assigned_blank, drop = FALSE]
       subtracted_columns <- od_matrix[, corresponding_wells, drop = FALSE] - broadcasted_assigned_blank
-      
-      # Add to od_values values
-      od_values[[blank_condition]] <- subtracted_columns
+      od_values[[blank_condition]] <- as.numeric(subtracted_columns)
       
       # Repeat for fluorescent data
       broadcasted_assigned_blank <- flu_matrix[, assigned_blank, drop = FALSE]
       subtracted_columns <- flu_matrix[, corresponding_wells, drop = FALSE] - broadcasted_assigned_blank
-      flu_values[[blank_condition]] <- subtracted_columns
+      flu_values[[blank_condition]] <- as.numeric(subtracted_columns)
     }
   }
   
-  # Arrange od_values and flu_values matrixes according to the order of experiments
+  # Arrange od_values and flu_values matrices according to the order of experiments
   unique_values_sorted <- tidy_format %>% distinct(Condition)
   od_values <- od_values[, match(unique_values_sorted$Condition, names(od_values))]
   flu_values <- flu_values[, match(unique_values_sorted$Condition, names(flu_values))]
   
-  # Assign values to new dataframe
+  # Assign values to the new dataframe
   tidy_format$od <- od_values %>% gather() %>% select(value) %>% pull()
   tidy_format$flu <- flu_values %>% gather() %>% select(value) %>% pull()
   
-  # Step N°3: calculate model parameters
+  # Assign replicate numbers for each condition
+  tidy_format <- tidy_format %>%
+    group_by(Condition) %>%
+    mutate(replicate = rep(1:max(table(design_matrix$Condition)), each = length(od_matrix$time))) %>%
+    ungroup()
+  
+  # Step N°3: Calculate model parameters
   
   tidy_format <- tidy_format %>%
     group_by(Condition) %>%
     mutate(
       phi = flu / od,
-      growth_rate = c(diff(od), NA) / c(od[-n()], NA) / diff(time),
-      production_rate = c(diff(flu), NA) / c(od[-n()], NA) / diff(time)
+      # Calculate growth_rate manually for the first time point, then use diff for the rest
+      growth_rate = c((od[2] - od[1]) / (time[2] - time[1]), diff(od) / diff(time)),
+      # Similarly, calculate production_rate manually for the first time point
+      production_rate = c((flu[2] - flu[1]) / (time[2] - time[1]), diff(flu) / diff(time))
     ) %>%
     ungroup()
   
+  # Apply smooth_filter to add 'gr' and 'pr' columns
+  tidy_format <- smooth_filter(tidy_format)
+  
+  # Change the column name from "Condition" to "inducer"
+  colnames(tidy_format)[colnames(tidy_format) == "Condition"] <- "inducer"
+  
   return(tidy_format)
 }
-
